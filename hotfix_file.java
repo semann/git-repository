31abebcafc77a6a0143ab54757cb3c2f95f3dd40
semann <semann.jeong@gmail.com>
2018-02-10 14:39:42 +0900

    rebase server onto master

--- hotfix_file.java
+++ hotfix_file.java
@@ -4,8 +4,13 @@ temp modify
 temp modify 2
 server modify 1
 <<<<<<< HEAD
+<<<<<<< HEAD
 cleint modify 1
 client modify 2
 =======
 server modify 2
 >>>>>>> server modify 2
+=======
+server modify 2
+server modify 3
+>>>>>>> server modify 3
